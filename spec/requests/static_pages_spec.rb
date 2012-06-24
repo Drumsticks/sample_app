39a4247b4feac4d5720b5a9937fbcade57130b78
Ryan Hinton <drummingsticks@gmail.com>
2012-06-24 19:19:39 -0400

    Added unversioned files.

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,77 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App"}
+
+  describe "Home page" do
+=begin
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_content 'Sample App'
+    end
+=end
+    it "should have the h1 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_selector 'h1', :text => 'Sample App'
+    end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector 'title', :text => "#{base_title} | Home"
+    end
+  end
+  describe "Help page" do
+=begin
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content 'Help'
+    end
+=end
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector 'h1', :text => 'Help'
+    end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector 'title', :text => "#{base_title} | Help"
+    end
+  end
+  describe "About page" do
+=begin
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      page.should have_content 'About Us'
+    end
+=end
+    it "should have the h1 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector 'h1', :text => 'About Us'
+    end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector 'title', :text => "#{base_title} | About Us"
+    end
+  end
+  describe "Contact page" do
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector 'h1', :text => 'Contact Us'
+    end
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector 'title', :text => "Ruby on Rails Tutorial Sample App | Contact Us"
+    end
+  end
+end
+
+#require 'spec_helper'
+
+#describe "StaticPages" do
+#  describe "GET /static_pages" do
+#    it "works! (now write some real specs)" do
+#      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+#      get static_pages_index_path
+#      response.status.should be(200)
+#    end
+#  end
+#end
